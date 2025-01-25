b50e074a753821c8d9b37f5f1eaaae28dc831516
guilherme-biscaino <guilhermebs.bs2@hotmail.com>
2025-01-24 22:30:16 -0300

    data handling refactored to use tensorflow automatic dataset optimizing machine resources and added performance metrics.

--- main.py
+++ main.py
@@ -1,98 +1,85 @@
 import os
 
-import random
 import numpy as np
-import keras
-
+import tensorflow as tf
+import pandas as pd
 import matplotlib.pyplot as plt
-from keras_preprocessing import image
-from tf_keras.applications.imagenet_utils import preprocess_input 
+
+from sklearn import metrics
+from sklearn.metrics import precision_recall_fscore_support
 from keras import layers
 from keras import Model
 from pathlib import Path
 
 root_path = Path(__file__).parent
 root = root_path / 'flowers/'
-train_split, val_split = 0.7, 0.15
+batch_size = 32
+img_width = 224
+img_height = 224
 
-categories = [x[0] for x in os.walk(root) if x[0]][1:]
-categories = [c for c in categories if c not in [os.path.join(root)]]
+# new data handling using tensorflow method folowing 'load and pre-process' tutorial
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    root,
+    validation_split=0.2,
+    subset='training',
+    seed=123,
+    image_size=(img_width, img_height),
+    batch_size=batch_size
+    )
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    root,
+    validation_split=0.2,
+    subset='validation',
+    seed=123,
+    image_size=(img_width, img_height),
+    batch_size=batch_size
+    )
+
+class_names = train_ds.class_names
+
+num_classes = len(class_names)
 
 
-def get_image(path):
-    img = image.load_img(path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return img, x
+# data normalization
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+normalized_train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+normalized_val_ds = val_ds.map(lambda x, y: (normalization_layer(x), y))
 
 
-data = []
-for c, category in enumerate(categories):
-    images = [os.path.join(dp, f) for dp, dn, filenames
-              in os.walk(category) for f in filenames
-              if os.path.splitext(f)[1].lower() in ['.jpg', '.png', '.jpeg']]
-    for img_path in images:
-        img, x = get_image(img_path)
-        data.append({'x': np.array(x[0]), 'y': c})
+# using tensorflow buffers for optimal performance
+AUTOTUNE = tf.data.AUTOTUNE
+prepared_train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+prepared_val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-num_classes = len(categories)
+# downloading vgg16 model
+base_model = tf.keras.applications.VGG16(weights='imagenet', include_top=True)
 
-random.shuffle(data)
+# freezing weights
+base_model.trainable = False
 
-idx_val = int(train_split * len(data))
-idx_test = int((train_split + val_split) * len(data))
-train = data[:idx_val]
-val = data[idx_val:idx_test]
-test = data[idx_test:]
+# adding new layers to enable training
+input = base_model.input
+new_layer = layers.Dense(num_classes, activation="softmax")
+output = new_layer(base_model.layers[-2].output)
+model_new = Model(input, output)
 
-x_train, y_train = np.array([t["x"] for t in train]), [t["y"] for t in train]
-x_val, y_val = np.array([t["x"] for t in val]), [t["y"] for t in val]
-x_test, y_test = np.array([t["x"] for t in test]), [t["y"] for t in test]
-
-# normalize data
-x_train = x_train.astype('float32') / 255.
-x_val = x_val.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-
-# convert labels to one-hot vectors
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_val = keras.utils.to_categorical(y_val, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-vgg = keras.applications.VGG16(weights='imagenet', include_top=True)
-
-# make a reference to VGG's input layer
-inp = vgg.input
-
-# make a new softmax layer with num_classes neurons
-new_classification_layer = layers.Dense(num_classes, activation='softmax')
-
-# connect our new layer to the second to last layer in VGG, and make a reference to it
-out = new_classification_layer(vgg.layers[-2].output)
-
-# create a new network between inp and out
-model_new = Model(inp, out)
-
-if os.path.exists(root_path / 'flowers.weights.h5'):
+# checking if model weights already exists otherwise train model again
+if os.path.exists('flowers.weights.h5'):
     model_new.load_weights('flowers.weights.h5')
+
+    model_new.compile(loss='sparse_categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
 else:
-    # make all layers untrainable by freezing weights (except for last layer)
-    for l, layer in enumerate(model_new.layers[:-1]):
-        layer.trainable = False
+    model_new.compile(loss='sparse_categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
 
-    # ensure the last layer is trainable/not frozen
-    for l, layer in enumerate(model_new.layers[-1:]):
-        layer.trainable = True
-
-    model_new.compile(loss='categorical_crossentropy',
-                    optimizer='adam',
-                    metrics=['accuracy'])
-
-    history2 = model_new.fit(x_train, y_train,
-                            batch_size=32,
-                            epochs=10,
-                            validation_data=(x_val, y_val))
+    history2 = model_new.fit(
+        prepared_train_ds,
+        batch_size=32,
+        epochs=10,
+        validation_data=prepared_val_ds)
 
     model_new.save_weights('flowers.weights.h5')
 
@@ -104,29 +91,40 @@ else:
 
     plt.show()
 
-target_shape = (224, 224)
+# manipulation needed to ease confusion matrix creation
+y_true = []
+y_pred = []
+for x, y in prepared_val_ds:
 
-def test_image(file_path, model):
-    img = image.load_img(file_path, target_size=target_shape)
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0
+    y_true.append(y)
+    y_pred.append(tf.argmax(model_new.predict(x), axis=1))
 
-    predictions = model.predict(img_array)
+y_pred = tf.concat(y_pred, axis=0)
+y_true = tf.concat(y_true, axis=0)
 
-    class_probabilities = predictions[0]
+# creating and printing metrics
+confusion_matrix = metrics.confusion_matrix(y_true, y_pred)
 
-    predicted_class_index = np.argmax(class_probabilities)
+confusion_matrix_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix)
+confusion_matrix_display.plot()
 
-    return class_probabilities, predicted_class_index
+plt.show()
 
+model_metrics = []
+for i, label in enumerate(class_names):
+    precision, recall, fscore, support = precision_recall_fscore_support(np.array(y_true) == i, np.array(y_pred) == i)
+    model_metrics.append([label, recall[0], recall[1], precision[1], fscore[1], support[1]])
 
-test_image_file = '/content/flowers/daisy/100080576_f52e8ee070_n.jpg' #image path for testing
-class_probabilities, predicted_class_index = test_image(test_image_file, model_new)
+df = pd.DataFrame(model_metrics, columns=["label", "especificidade",  "sensibilidade", "precisão", "f_score", "suporte"])
 
-for i, class_label in enumerate(categories):
-    probability = class_probabilities[i]
-    print(f'Class: {class_label}, Probability: {probability:.4f}')
+print("=" * 50)
+print("Metricas de cada classe")
+print(df)
+print("=" * 50)
 
-predicted_class = categories[predicted_class_index]
-print(f'The image is classified as: {predicted_class}')
+weight = df["suporte"]/df["suporte"].sum()
+df_medium = df[["especificidade",  "sensibilidade", "precisão", "f_score"]].apply(lambda col: np.sum(col*weight, axis=0))
+print("="*50)
+print("Metrica das médias entre as classes")
+print(df_medium)
+print("="*50)
